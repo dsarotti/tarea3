0b6d71f69b70365c00a67c6bb6b56169fca0d3ae
dsarotti <sr.sarotti@gmail.com>
2023-02-02 13:32:22 +0100

    tiempos

--- Herramientas.java
+++ Herramientas.java
@@ -5,62 +5,108 @@ import java.util.ListIterator;
 public class Herramientas {
 
     /**
-     * Inserta @param size elementos en un ArrayList.
+     * Mide en nanosegundos el tiempo que se tarda en insertar @param size elementos
+     * en un ArrayList.
      * 
+     * @param inicioTimer
+     * @param finTimer
      * @param arrayList
      * @param size
+     * @return el tiempo que se ha tardado.
      */
-    public static void inserciones(ArrayList<Integer> arrayList, int size) {
+    public static long medirInserciones(ArrayList<Integer> arrayList, int size) {
+        long inicioTimer;
+        long finTimer;
+        long acumulado = 0;
         for (int i = 0; i < size; i++) {
+            inicioTimer = System.nanoTime();
             arrayList.add(i);
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
         }
+        return acumulado;
     }
 
     /**
-     * Inserta @param size elementos en una lista enlazada.
+     * Mide en nanosegundos el tiempo que se tarda en insertar @param size elementos
+     * en una lista enlazada.
      * 
+     * @param inicioTimer
+     * @param finTimer
      * @param lList
      * @param size
+     * @return el tiempo que se ha tardado.
      */
-    public static void inserciones(LinkedList<Integer> lList, int size) {
+    public static long medirInserciones(LinkedList<Integer> lList, int size) {
+        long inicioTimer;
+        long finTimer;
+        long acumulado = 0;
+
         for (int i = 0; i < size; i++) {
+            inicioTimer = System.nanoTime();
             lList.add(i);
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
         }
+        return acumulado;
     }
 
     /**
-     * Accede a todos los elementos de un ArrayList empezando por el final.
+     * Mide en nanosegundos el tiempo que se tarda en acceder a los elementos en un
+     * ArrayList.
      * 
      * @param arrayList
      * @param size
+     * @return el tiempo que se ha tardado.
      */
-    public static void accesosInverso(ArrayList<Integer> arrayList, int size) {
-        for (int i = size-1; i >= 0; i--) {
+    public static long medirAccesos(ArrayList<Integer> arrayList) {
+        long inicioTimer;
+        long finTimer;
+        long acumulado = 0;
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+            inicioTimer = System.nanoTime();
             arrayList.get(i);
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
         }
+        return acumulado;
     }
 
     /**
-     * Accede a todas los elementos de una lista enlazada empezando por el final.
+     * Mide en nanosegundos el tiempo que se tarda en acceder a los elementos en una
+     * lista enlazada.
      * 
      * @param lList
+     * @return el tiempo que se ha tardado.
      */
-    public static void accesosInverso(LinkedList<Integer> lList) {
+    public static long medirAccesos(LinkedList<Integer> lList) {
+        long inicioTimer;
+        long finTimer;
+        long acumulado = 0;
         ListIterator<Integer> iterador = lList.listIterator();
-        while (iterador.hasNext())
+        while (iterador.hasNext()) {
+            inicioTimer = System.nanoTime();
             iterador.next();
-        while (iterador.hasPrevious())
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
+        }
+        while (iterador.hasPrevious()) {
+            inicioTimer = System.nanoTime();
             iterador.previous();
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
+        }
+        return acumulado;
     }
 
     /**
      * Devuelve en nanosegundos el tiempo que se tarda en ejecutar el método
-     * contains sobre todos los elementos de un ArrayList
+     * contains sobre todos los elementos de un ArrayList.
      * 
      * @param arrayList
-     * @return
+     * @return el tiempo que se ha tardado.
      */
-    public static long pruebaContains(ArrayList<Integer> arrayList) {
+    public static long medirContains(ArrayList<Integer> arrayList) {
         long acumulado = 0;
         long inicioTimer;
         long finTimer;
@@ -78,9 +124,9 @@ public class Herramientas {
      * contains sobre todos los elementos de una lista enlazada.
      * 
      * @param LList
-     * @return
+     * @return el tiempo que se ha tardado.
      */
-    public static long pruebaContains(LinkedList<Integer> lList) {
+    public static long medirContains(LinkedList<Integer> lList) {
         long acumulado = 0;
         long inicioTimer;
         long finTimer;
@@ -94,71 +140,83 @@ public class Herramientas {
     }
 
     /**
-     * Mide en nanosegundos el tiempo que se tarda en insertar @param size elementos
-     * en un ArrayList.
-     * 
-     * @param inicioTimer
-     * @param finTimer
-     * @param arrayList
-     * @param size
-     * @return
-     */
-    public static long medirInserciones(ArrayList<Integer> arrayList, int size) {
-        long inicioTimer;
-        long finTimer;
-        inicioTimer = System.nanoTime();
-        inserciones(arrayList, size);
-        finTimer = System.nanoTime();
-        return (finTimer - inicioTimer);
-    }
-
-    /**
-     * Mide en nanosegundos el tiempo que se tarda en insertar @param size elementos
-     * en una lista enlazada.
-     * 
-     * @param inicioTimer
-     * @param finTimer
-     * @param lList
-     * @param size
-     * @return
-     */
-    public static long medirInserciones(LinkedList<Integer> lList, int size) {
-        long inicioTimer;
-        long finTimer;
-        inicioTimer = System.nanoTime();
-        inserciones(lList, size);
-        finTimer = System.nanoTime();
-        return (finTimer - inicioTimer);
-    }
-
-    /**
-     * Mide en nanosegundos el tiempo que se tarda en acceder a los elementos en un
-     * ArrayList.
+     * Mide en nanosegundos el tiempo que se tarda en eliminar todos los elementos
+     * de un ArrayList utilizando remove() y empezando por el principio.
      * 
      * @param arrayList
-     * @param size
-     * @return
+     * @return el tiempo que se ha tardado.
      */
-    public static long medirAccesos(ArrayList<Integer> arrayList, int size) {
-        long inicioTimer = System.nanoTime();
-        accesosInverso(arrayList, size);
-        long finTimer = System.nanoTime();
-        return (finTimer - inicioTimer);
+    public static long medirEliminaciones(ArrayList<Integer> arrayList) {
+        long acumulado = 0;
+        long inicioTimer;
+        long finTimer;
+        for (int i = 0; i < arrayList.size(); i++) {
+            inicioTimer = System.nanoTime();
+            arrayList.remove(0);
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
+        }
+        return acumulado;
     }
 
     /**
-     * Mide en nanosegundos el tiempo que se tarda en acceder a los elementos en una
-     * lista enlazada.
+     * Mide en nanosegundos el tiempo que se tarda en eliminar todos los elementos
+     * de una lista enlazada utilizando removeFirst().
      * 
-     * @param lList
-     * @param size
-     * @return
+     * @param arrayList
+     * @return el tiempo que se ha tardado.
      */
-    public static long medirAccesos(LinkedList<Integer> lList, int size) {
-        long inicioTimer = System.nanoTime();
-        accesosInverso(lList);
-        long finTimer = System.nanoTime();
-        return (finTimer - inicioTimer);
+    public static long medirEliminaciones(LinkedList<Integer> lList) {
+        long acumulado = 0;
+        long inicioTimer;
+        long finTimer;
+        for (int i = 0; i < lList.size(); i++) {
+            inicioTimer = System.nanoTime();
+            lList.removeFirst();
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
+        }
+        return acumulado;
+    }
+
+    /**
+     * Mide en nanosegundos el tiempo que se tarda en eliminar todos los elementos
+     * de un ArrayList utilizando remove() y empezando por el final.
+     * 
+     * @param arrayList
+     * @return el tiempo que se ha tardado.
+     */
+    public static long medirEliminacionesInversa(ArrayList<Integer> arrayList) {
+        long acumulado = 0;
+        long inicioTimer;
+        long finTimer;
+        for (int i = arrayList.size()-1; i >= 0; i--) {
+            inicioTimer = System.nanoTime();
+            arrayList.remove(i);
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
+        }
+        return acumulado;
+    }
+
+    /**
+     * Mide en nanosegundos el tiempo que se tarda en eliminar todos los elementos
+     * de una lista enlazada utilizando removeLast().
+     * 
+     * @param arrayList
+     * @return el tiempo que se ha tardado.
+     */
+    public static long medirEliminacionesInversa(LinkedList<Integer> lList) {
+        long acumulado = 0;
+        long inicioTimer;
+        long finTimer;
+        for (int i = 0; i < lList.size(); i++) {
+            inicioTimer = System.nanoTime();
+            lList.removeLast();
+            finTimer = System.nanoTime();
+            acumulado = acumulado + (finTimer - inicioTimer);
+        }
+        return acumulado;
     }
 
     /**
@@ -166,7 +224,7 @@ public class Herramientas {
      * 
      * @param tiempos
      * @param repeticiones
-     * @return
+     * @return la media de los valores.
      */
     public static double media(long[] tiempos, int repeticiones) {
         long tiempoAcumulado = 0;
--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 import java.io.Console;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,9 +13,6 @@ public class Main {
 
         ArrayList<Integer> arrList = new ArrayList<>();
         LinkedList<Integer> linList = new LinkedList<>();
-
-        long inicioTimer = 0;
-        long finTimer = 0;
 
         // pedir tamaño de las listas con el que se va a trabajar.
         System.out.print("Indica el tamaño de las listas: ");
@@ -61,7 +55,7 @@ public class Main {
         // Pruebas: Acceso en sentido inverso con ArrayList
         System.out.println("__________ACCESOS__________");
         for (int i = 0; i < REPETICIONES; i++)
-            tiempos[i] = Herramientas.medirAccesos(arrList, SIZE);
+            tiempos[i] = Herramientas.medirAccesos(arrList);
         // // calcular media:
         media = Herramientas.media(tiempos, REPETICIONES);
         tiempos = new long[REPETICIONES];
@@ -71,7 +65,7 @@ public class Main {
 
         // Pruebas: Acceso en sentido inverso con LinkedList
         for (int i = 0; i < REPETICIONES; i++)
-            tiempos[i] = Herramientas.medirAccesos(linList, SIZE);
+            tiempos[i] = Herramientas.medirAccesos(linList);
         // // calcular media
         media = Herramientas.media(tiempos, REPETICIONES);
         tiempos = new long[REPETICIONES];
@@ -85,10 +79,10 @@ public class Main {
         // Para esta prueba utilizaremos un valor incremental para cada iteración, que
         // es el elemento que debería haber en cada iteración.
 
-        // Pruebas: contains() en con ArrayList
+        // Pruebas: contains() en ArrayList
         System.out.println("__________contains()__________");
         for (int i = 0; i < REPETICIONES; i++) {
-            tiempos[i] = Herramientas.pruebaContains(arrList);
+            tiempos[i] = Herramientas.medirContains(arrList);
         }
         // // calcular media
         media = Herramientas.media(tiempos, REPETICIONES);
@@ -97,10 +91,9 @@ public class Main {
         System.out.println("ArrayList - Media calculada al ejecutar contains() de todos los elementos " + REPETICIONES
                 + " veces: " + media + " nanosegundos.");
 
-        // Pruebas: contains() en con LinkedList
-        System.out.println("__________contains()__________");
+        // Pruebas: contains() con LinkedList
         for (int i = 0; i < REPETICIONES; i++) {
-            tiempos[i] = Herramientas.pruebaContains(linList);
+            tiempos[i] = Herramientas.medirContains(linList);
         }
         // // calcular media
         media = Herramientas.media(tiempos, REPETICIONES);
@@ -109,6 +102,59 @@ public class Main {
         System.out.println("LinkedList - Media calculada al ejecutar contains() de todos los elementos " + REPETICIONES
                 + " veces: " + media + " nanosegundos.");
 
+        // -------------------------------------------------------------------
+
+        // -----------------------Pruebas de eliminación----------------------
+        // Eliminar los elementos de la lsita comenzando por el principio y por el
+        // final.
+
+        System.out.println("__________ELIMINACIONES__________");
         
+        // Pruebas: eleminacion empezando por el principio en ArrayList
+        for (int i = 0; i < REPETICIONES; i++) {
+            tiempos[i] = Herramientas.medirEliminaciones(arrList);
+        }
+        // // calcular media
+        media = Herramientas.media(tiempos, REPETICIONES);
+        tiempos = new long[REPETICIONES];
+        // reporte:
+        System.out.println("ArrayList - Media calculada al eliminar todos los elementos empezando por el principio: "
+                + media + " nanosegundos.");
+
+        // Pruebas: eleminacion empezando por el final en ArrayList
+        Herramientas.medirInserciones(arrList, SIZE); // utilizo este método para volver a llenar el arrayList
+        for (int i = 0; i < REPETICIONES; i++) {
+            tiempos[i] = Herramientas.medirEliminacionesInversa(arrList);
+        }
+        // // calcular media
+        media = Herramientas.media(tiempos, REPETICIONES);
+        tiempos = new long[REPETICIONES];
+        // reporte:
+        System.out.println("ArrayList - Media calculada al eliminar todos los elementos empezando por el final: "
+                + media + " nanosegundos.");
+
+        
+        // Pruebas: eleminacion empezando por el principio en LinkedList
+        for (int i = 0; i < REPETICIONES; i++) {
+            tiempos[i] = Herramientas.medirEliminaciones(linList);
+        }
+        // // calcular media
+        media = Herramientas.media(tiempos, REPETICIONES);
+        tiempos = new long[REPETICIONES];
+        // reporte:
+        System.out.println("LinkedList - Media calculada al eliminar todos los elementos empezando por el principio: "
+                + media + " nanosegundos.");
+
+        // Pruebas: eleminacion empezando por el final en LinkedList
+        Herramientas.medirInserciones(linList, SIZE); // utilizo este método para volver a llenar la LinkedList
+        for (int i = 0; i < REPETICIONES; i++) {
+            tiempos[i] = Herramientas.medirEliminacionesInversa(linList);
+        }
+        // // calcular media
+        media = Herramientas.media(tiempos, REPETICIONES);
+        // reporte:
+        System.out.println("LinkedList - Media calculada al eliminar todos los elementos empezando por el final: "
+                + media + " nanosegundos.");
+
     }
 }
